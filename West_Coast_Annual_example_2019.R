3bb9fbf6a3b61004f7402fa28186c2794ca96471
John Wallace <John.Wallace@noaa.gov>
2019-06-10 14:51:24 -0700

    Added gitAFile and gitEdit info

--- West_Coast_Annual_example_2019.R
+++ West_Coast_Annual_example_2019.R
@@ -1,4 +1,10 @@
 
+# Download with:
+JRWToolBox::gitAFile("John-R-Wallace-NOAA/VAST_Examples_and_Scripts/master/West_Coast_Annual_example_2019.R", "script", File = "West_Coast_Annual_example_2019.R", show = FALSE)
+# or edit with [using a properly configured gitEdit()]
+JRWToolBox::gitEdit(West_Coast_Annual_example_2019, "John-R-Wallace-NOAA/VAST_Examples_and_Scripts/master/")
+
+
 # Test run of single species spatial delta glmm
 # Test, canary data; implementation, Lingcod groundfish survey data
 # Based on single-species example
