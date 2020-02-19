c8316ae36051a0521dada93318e89e35034b997c
John Wallace <John.Wallace@noaa.gov>
2020-02-18 16:47:16 -0800

    Bug fixes (perhaps none)

--- West_Coast_Annual_Exmpl_2020_FS.R
+++ West_Coast_Annual_Exmpl_2020_FS.R
@@ -1,5 +1,4 @@
 
-
 # Download with:
 # JRWToolBox::gitAFile("John-R-Wallace-NOAA/VAST_Examples_and_Scripts/master/West_Coast_Annual_Exmpl_2020_FS.R", "script", File = "West_Coast_Annual_example_2019.R", show = FALSE)
 # or edit with [using a properly configured gitEdit()]
