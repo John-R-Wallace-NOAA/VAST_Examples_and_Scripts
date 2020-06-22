dcc01ed5ce333595a1de413ecb63f553b4b1431b
John Wallace <John.Wallace@noaa.gov>
2020-06-22 16:51:18 -0700

    Update West_Coast_Example_2020_V3X.R

--- West_Coast_Example_2020_V3X.R
+++ West_Coast_Example_2020_V3X.R
@@ -1,8 +1,8 @@
 
 # Download into your working directory with:
-# JRWToolBox::gitAFile("John-R-Wallace-NOAA/VAST_Examples_and_Scripts/master/West_Coast_Example_2020_V3X.R", "script", File = "West_Coast_Example_2020_V3X.R", show = FALSE)
+# rgit::gitAFile("John-R-Wallace-NOAA/VAST_Examples_and_Scripts/master/West_Coast_Example_2020_V3X.R", "script", File = "West_Coast_Example_2020_V3X.R", show = FALSE)
 # or edit with [using a properly configured gitEdit()]
-# JRWToolBox::gitEdit(West_Coast_Example_2020_V3X, "John-R-Wallace-NOAA/VAST_Examples_and_Scripts/master/")
+# rgit::gitEdit(West_Coast_Example_2020_V3X, "John-R-Wallace-NOAA/VAST_Examples_and_Scripts/master/")
 
 
 # Test run of single species spatial delta glmm
