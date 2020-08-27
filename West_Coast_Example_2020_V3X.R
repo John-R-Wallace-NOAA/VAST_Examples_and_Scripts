39c36e479a1d9433f229c8e68032d74e6b00ee91
John Wallace <John.Wallace@noaa.gov>
2020-08-27 13:30:30 -0700

    Tantalus instructions

--- West_Coast_Example_2020_V3X.R
+++ West_Coast_Example_2020_V3X.R
@@ -473,10 +473,16 @@ West_Coast_Example_2020_V3X <- function(spFormalName = 'lingcod', spLongName = '
      # Setting formulaDepthSpline = TRUE or formulaDepth = TRUE will result in somewhat less smooth figures, but not as grainy as anything done with VAST 3.4 or VAST 3.5
      # [ The important thing is not using the formula argument, not what ObsModel was used. ]
      
-     # Default formula (~0) model
+    # On Tantalus - Default formula (~0) model 
+     library(JRWToolBox)
+     Linux.First()
+     
+     repoPath <- "John-R-Wallace-NOAA/VAST_Examples_and_Scripts"
+     rgit::S(West_Coast_Example_2020_V3X, subDir = NULL, show = FALSE)  # fix(West_Coast_Example_2020_V3X) could then be used to edit
+     str(West_Coast_Example_2020_V3X)  # Check 'lastEdit' arg for correct timestamp
+     
      West_Coast_Example_2020_V3X(spFormalName = 'Pacific spiny dogfish', spLongName = 'Spiny dogfish', spShortName = 'DSRK', ObsModel = c(1, 1), 
              depthCov = TRUE, formulaDepthSpline = FALSE, formulaDepth = FALSE)
-     
    }
 
 }
