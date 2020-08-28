ea9db60e6ff10909d4fa9b3c2ac2919979a3a34f
John Wallace <John.Wallace@noaa.gov>
2020-08-27 19:42:37 -0700

    This function gits itself for a record
    of what was run. (gits itself from GitHub)

--- West_Coast_Example_2020_V3X.R
+++ West_Coast_Example_2020_V3X.R
@@ -2,9 +2,9 @@
 West_Coast_Example_2020_V3X <- function(spFormalName = 'lingcod', spLongName = 'Lingcod', spShortName = 'LCOD', ObsModel. = c(2, 0), n_x. = 250, fine_scale. = TRUE, depthCov = TRUE,
               formulaDepthSpline = TRUE, formulaDepth = FALSE, lastEdit = "2020-08-27 14:49:56 PDT") {
   
-   # # Download into your .GlobalEnv with:
-   # repoPath <- "John-R-Wallace-NOAA/VAST_Examples_and_Scripts"
-   # rgit::S(West_Coast_Example_2020_V3X, subDir = NULL, show = FALSE)  # fix(West_Coast_Example_2020_V3X) could then be used to edit
+   # Download this function into your current environment, this will be saved in Image.RData for a record of what was run (see below):
+   repoPath <- "John-R-Wallace-NOAA/VAST_Examples_and_Scripts"
+   rgit::S(West_Coast_Example_2020_V3X, subDir = NULL, show = FALSE)  # fix(West_Coast_Example_2020_V3X) could then be used to edit
    
    # # Put directly into Notepad++ for editing with:
    # repoPath <- "John-R-Wallace-NOAA/VAST_Examples_and_Scripts"
