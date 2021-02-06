f517f3feae6847d741d18925d2923061d3e7c97c
John Wallace <John.Wallace@noaa.gov>
2021-02-05 16:15:31 -0800

    fixed and updated local 'Makevars' file code

--- West_Coast_Annual_example_2020.R
+++ West_Coast_Annual_example_2020.R
@@ -257,18 +257,18 @@ if(.Platform$OS.type == "windows" ) {
 
    # Create a local 'Makevars' file without the all warnings, '-Wall', flag  
    V <- Version
-   Ri <- file.path(paste0(R.home(), '/include'))
-   Ti <- file.path(paste0(R.home(), '/library/TMB/include'))
-   x64 <- file.path(paste0(R.home(), '/bin/x64'))
-        
-   sink("Makevars")
-     cat(paste0('\n\n', V, '.o: ', V, '.cpp\n'))
-     cat(paste0('\tC:/rtools40/mingw64/bin/g++ -std=gnu++11 -I"', Ri, '" -DNDEBUG -I"', Ti, '" -DTMB_SAFEBOUNDS -DLIB_UNLOAD=R_unload_', V, 
-                ' -DTMB_LIB_INIT=R_init_', V, ' -O2 -mfpmath=sse -msse2 -mstackrealign -c ', V, '.cpp -o ', V, '.o\n'))
-     cat(paste0('\tC:/rtools40/mingw64/bin/g++ -std=gnu++11 -shared -s -static-libgcc -o ', V, '.dll ',  V, '.o -L', x64,' -lR\n')) 
+   xF <- ""
+   Ri <- paste0(R.home(), '/include')
+   Ti <- system.file("include", package = "TMB")
+   Arch <- paste0(R.home(), '/bin', Sys.getenv("R_ARCH"))
+       
+   sink(paste0(DateFile, "Makevars"))
+      cat(paste0('\n\n', V, '.o: ', V, '.cpp\n'))
+      cat(paste0('\tC:/rtools40/mingw64/bin/g++ -std=gnu++11 -I"', Ri, '" -DNDEBUG -I"', Ti, '" -DTMB_SAFEBOUNDS -DLIB_UNLOAD=R_unload_', V, 
+                 ' -DTMB_LIB_INIT=R_init_', V, ' -O2 -mfpmath=sse -msse2 -mstackrealign -c ', xF, ' ', V, '.cpp -o ', V, '.o\n'))
+      cat(paste0('\tC:/rtools40/mingw64/bin/g++ -std=gnu++11 -shared -s -static-libgcc -o ', V, '.dll ',  V, '.o -L', Arch,' -lR\n')) 
    sink()
 }
-  
 
 # Build tmb object
 TmbList = VAST::make_model(TmbData = TmbData, RunDir = DateFile, Version = Version, RhoConfig = RhoConfig, loc_x = Spatial_List$loc_x, Method = Method)
