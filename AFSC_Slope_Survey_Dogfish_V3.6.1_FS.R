be2d48b5e1290cfb80c2b6802b7b94036d0fcf06
John Wallace <John.Wallace@noaa.gov>
2021-02-12 17:41:28 -0800

    little rearrangement

--- AFSC_Slope_Survey_Dogfish_V3.6.1_FS.R
+++ AFSC_Slope_Survey_Dogfish_V3.6.1_FS.R
@@ -10,54 +10,58 @@ sourceFunctionURL <- function (URL)
       write(paste(readLines(textConnection(httr::content(getTMP))), collapse = "\n"), File.ASCII)
       source(File.ASCII)
    }
-   
-sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/dataWareHouseTrawlCatch.R")
-sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/VAST_Examples_and_Scripts/master/R/West_Coast_2021_V3.6.1.R")  
     
 require(VAST)
-  
+ 
+ 
 # ============ Create a figure to view VAST's 'surveyname' grids (i.e. Domains) for CA Current region and plot the raw data on top of each one ====================
 
-  
-      WC <- FishStatsUtils:::Prepare_WCGBTS_Extrapolation_Data_Fn()
-      
-      WC$Data_Extrap[1:3,]
-      
-      Data_Set <- dataWareHouseTrawlCatch('Pacific spiny dogfish', yearRange =  c(1997, 2001), project = 'AFSC.Slope')
-           
-      Imap::imap(longlat = list(world.h.land, world.h.borders), col = c("black", "cyan"), poly = c("grey40", NA), longrange = c(-146, -115), latrange = c(32, 49), zoom = FALSE)
-      
-      change(WC$Data_Extrap[as.logical(WC$Data_Extrap$propInWCGBTS), ])
-      points(Lon, Lat, col = 'red', pch = '.')
-      text(-124.3, 36.67, "propInWCGBTS", cex = 0.6)
-      
-      change(WC$Data_Extrap[as.logical(WC$Data_Extrap$propInCCA), ])
-      points(Lon, Lat, col = 'yellow', pch = '.')
-      points(Data_Set$Longitude_dd, Data_Set$Latitude_dd, pch = '.', cex = 2)
-      
-       
-      change(WC$Data_Extrap[as.logical(WC$Data_Extrap$propInTriennial), ])
-      points(Lon - 5, Lat, col = 'blue', pch = '.')
-      points(Data_Set$Longitude_dd - 5, Data_Set$Latitude_dd, pch = '.', cex = 2)
-      text(-129.2, 36.67, "propInTriennial", cex = 0.6)
-      
-      change(WC$Data_Extrap[as.logical(WC$Data_Extrap$propInSlope98_00), ])
-      points(Lon - 10, Lat, col = 'green', pch = '.')
-      points(Data_Set$Longitude_dd - 10, Data_Set$Latitude_dd, pch = '.', cex = 2)
-      text(-134.37, 36.67, "propInSlope98_00", cex = 0.6)
-      
-      change(WC$Data_Extrap[as.logical(WC$Data_Extrap$propInSlope01), ])
-      points(Lon - 15, Lat, col = 'cyan', pch = '.')
-      points(Data_Set$Longitude_dd - 15, Data_Set$Latitude_dd, pch = '.', cex = 2)
-      text(-139.0, 36.67, "propInSlope01", cex = 0.6)
-      
-      change(WC$Data_Extrap[as.logical(WC$Data_Extrap$propInSlope02), ])
-      points(Lon - 20, Lat, col = 'purple', pch = '.')
-      points(Data_Set$Longitude_dd - 20, Data_Set$Latitude_dd, pch = '.', cex = 2)
-      text(-144.3, 36.67, "propInSlope02", cex = 0.6)
+sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/dataWareHouseTrawlCatch.R") 
+ 
+ 
+WC <- FishStatsUtils:::Prepare_WCGBTS_Extrapolation_Data_Fn()
+
+WC$Data_Extrap[1:3,]
+
+Data_Set <- dataWareHouseTrawlCatch('Pacific spiny dogfish', yearRange =  c(1997, 2001), project = 'AFSC.Slope')
+     
+Imap::imap(longlat = list(world.h.land, world.h.borders), col = c("black", "cyan"), poly = c("grey40", NA), longrange = c(-146, -115), latrange = c(32, 49), zoom = FALSE)
+
+change(WC$Data_Extrap[as.logical(WC$Data_Extrap$propInWCGBTS), ])
+points(Lon, Lat, col = 'red', pch = '.')
+text(-124.3, 36.67, "propInWCGBTS", cex = 0.6)
+
+change(WC$Data_Extrap[as.logical(WC$Data_Extrap$propInCCA), ])
+points(Lon, Lat, col = 'yellow', pch = '.')
+points(Data_Set$Longitude_dd, Data_Set$Latitude_dd, pch = '.', cex = 2)
+
+ 
+change(WC$Data_Extrap[as.logical(WC$Data_Extrap$propInTriennial), ])
+points(Lon - 5, Lat, col = 'blue', pch = '.')
+points(Data_Set$Longitude_dd - 5, Data_Set$Latitude_dd, pch = '.', cex = 2)
+text(-129.2, 36.67, "propInTriennial", cex = 0.6)
+
+change(WC$Data_Extrap[as.logical(WC$Data_Extrap$propInSlope98_00), ])
+points(Lon - 10, Lat, col = 'green', pch = '.')
+points(Data_Set$Longitude_dd - 10, Data_Set$Latitude_dd, pch = '.', cex = 2)
+text(-134.37, 36.67, "propInSlope98_00", cex = 0.6)
+
+change(WC$Data_Extrap[as.logical(WC$Data_Extrap$propInSlope01), ])
+points(Lon - 15, Lat, col = 'cyan', pch = '.')
+points(Data_Set$Longitude_dd - 15, Data_Set$Latitude_dd, pch = '.', cex = 2)
+text(-139.0, 36.67, "propInSlope01", cex = 0.6)
+
+change(WC$Data_Extrap[as.logical(WC$Data_Extrap$propInSlope02), ])
+points(Lon - 20, Lat, col = 'purple', pch = '.')
+points(Data_Set$Longitude_dd - 20, Data_Set$Latitude_dd, pch = '.', cex = 2)
+text(-144.3, 36.67, "propInSlope02", cex = 0.6)
+
       
        
 # ================================ Setup and run the model ============================================
+
+sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/VAST_Examples_and_Scripts/master/R/West_Coast_2021_V3.6.1.R")   
+ 
  
 (strata.limits <- data.frame(
        STRATA = c("Coastwide","CA","OR","WA"),
